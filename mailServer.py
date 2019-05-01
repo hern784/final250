f940185dfe4de92b97c3c9cdad0583a798123ab5
Richard Hernandez <hern784@usc.edu>
2019-05-01 01:42:55 -0700

    http

--- mailServer.py
+++ mailServer.py
@@ -8,61 +8,6 @@ import mailboxManager
 
 app = Flask('RaspberryPi Mailbox Server')
 
-"""
-The @app.route() above the function is called a decorator. We will skip
-explaining decorators in detail for brevity. The functions below, such as
-get_mailbox_callback(), are callbacks that get called when certain types
-of HTTP request are received by the Flask server. With the decorator's
-input arguments below and the Flask server initialization in
-if __name__ == '__main__':, this first callback is set to be specifically
-called when a GET request is sent to the URL "http://0.0.0.0:[port]/mailbox"
-"""
-
-@app.route('/mailbox', methods=['GET'])
-def get_mailbox_callback():
-    """
-    Summary: A callback which for when GET is called on [host]:[port]/mailbox
-
-    Returns:
-        string: A JSON-formatted string containing the response message
-    """
-
-    # Since we have `from flask import request` above, the 'request' object
-    # will (magically) be available when the callback is called. `request` is
-    # the object that stores all the HTTP message data (header, payload, etc.).
-    # We will skip explaining how this object gets here because the answer is
-    # a bit long and out of the scope of this lab.
-
-    # Extract the key/value field for password
-    password = request.args.get('password')
-
-    # Check that the password is valid
-    if password == mailbox_password:
-        # Use Flask's jsonify function to format the dictionary as JSON
-        response = jsonify(mailbox_manager.get_mail())
-        incomming_temp = payload["temp"]
-        print(incomming_temp)
-
-    else:
-        if password == None:
-            response = jsonify({'Response': 'Missing password'})
-
-        else:
-            response = jsonify({'Response': 'Password does not match'})
-
-    # The object returned will be sent back as an HTTP message to the requester
-    return response
-
-# TODO: Use Flash's route() decorator to add support to your HTTP server for
-# handling GET requests made to the URL '/mailbox/search'
-#
-# Use get_mailbox_callback() as an example. You'll need to use mailboxManager
-# for this request as well, so make sure to spend some time understanding how
-# it works and the features it provides.
-#
-# Your implementation should handle reasonable error cases as well, such as an
-# incorrect password.
-
 @app.route('/mailbox/search', methods=['GET'])
 def search_mailbox_callback():
     password = request.args.get('password')
@@ -72,6 +17,16 @@ def search_mailbox_callback():
 
     if password == mailbox_password:
         response = jsonify(mailbox_manager.get_mail(field, text))
+        payload = request.get_json()
+        in_temp = payload["temp"]
+        print(in_temp)
+        print(payload)
+
+        mailbox_manager.add_mail(payload)
+        response = {'Response': 'Mail sent'}
+
+    # The object returned will be sent back as an HTTP message to the requester
+        return json.dumps(response)
         
     else:
         if password == None:
@@ -80,36 +35,7 @@ def search_mailbox_callback():
         else:
             response = jsonify({'Response': 'Password does not match'})
 
-    return response      
-
-
-@app.route('/mailbox/delete', methods=['DELETE'])
-def delete_mail_callback():
-    """
-    Summary: A callback for when DELETE is called on [host]:[port]/mailbox/delete
-
-    Returns:
-        string: A JSON-formatted string containing the response message
-    """
-
-    # Get the payload containing the list of mail ids to delete
-    payload = request.get_json()
-    print(payload)
-
-    # Check that the password is valid
-    if payload['password'] == mailbox_password:
-        num_deleted = mailbox_manager.delete_mail(payload['mail_ids'])
-        response = jsonify({'Response': '{} emails deleted'.format(num_deleted)})
-
-    else:
-        if password == None:
-            response = jsonify({'Response': 'Missing password'})
-
-        else:
-            response = jsonify({'Response': 'Password does not match'})
-
-    # The object returned will be sent back as an HTTP message to the requester
-    return response
+    return response  
 
 @app.route('/send-mail', methods=['POST'])
 def post_mail_callback():
@@ -122,9 +48,9 @@ def post_mail_callback():
 
     # Get the payload containing the sender, subject and body parameters
     payload = request.get_json()
-    incomming_temp = payload["temp"]
+    in_temp = payload["temp"]
+    print(in_temp)
     print(payload)
-    print(incomming_temp)
 
     mailbox_manager.add_mail(payload)
     response = {'Response': 'Mail sent'}
@@ -145,7 +71,7 @@ if __name__ == '__main__':
     mailbox_password = args.p   # password
     mailbox_manager = mailboxManager.mailboxManager()
 
-    app.run(debug=False, host='0.0.0.0', port=5005)
+    app.run(debug=False, host='0.0.0.0', port=5055)
 
 
 
