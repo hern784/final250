95111e10c6f2390fd1472ac340b6c33936102928
Richard Hernandez <hern784@usc.edu>
2019-05-01 20:15:39 -0700

    security

--- mailServer.py
+++ mailServer.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     mailbox_password = args.p   # password
     mailbox_manager = mailboxManager.mailboxManager()
 
-    app.run(debug=False, host='0.0.0.0', port=5570)
+    app.run(debug=False, host='localhost', port=5570)
 
 
 
