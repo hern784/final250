1b64bf7dad00029900d98a6b231712eeb9f01931
Richard Hernandez <hern784@usc.edu>
2019-05-02 16:47:31 -0700

    debug

--- final.py
+++ final.py
@@ -326,7 +326,7 @@ if __name__ == '__main__':
     mailbox_password = args.p   # password
     mailbox_manager = mailboxManager.mailboxManager()
     startup()
-    app.run(debug=True, host='0.0.0.0', port=4250)
+    app.run(debug=True, host='0.0.0.0', port=4251)
 
 
 
