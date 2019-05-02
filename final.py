b70ab2971da6a5cfe54105c5845327add749fc35
Richard Hernandez <hern784@usc.edu>
2019-05-02 01:00:59 -0700

    added mailserver to main

--- final.py
+++ final.py
@@ -23,31 +23,6 @@ import argparse
 import json
 import mailboxManager
 
-app = Flask('RaspberryPi Mailbox Server')
-
-
-@app.route('/send-mail', methods=['POST'])
-def post_mail_callback():
-
-    global in_temp2
-
-    payload = request.get_json()
-    in_temp = int(payload["temp"])
-
-    # Print incomming temp
-    print("Incomming encrypted temp = " + str(in_temp))
-    in_temp2 = math.sqrt(in_temp)
-    print("Incomming decrypted temp = " + str(in_temp2))
-    print(payload)
-
-    if (in_temp2>60) and (in_temp2 <100):
-        mailbox_manager.add_mail(payload)
-        response = {'Response': 'Mail sent', 'Recieved': in_temp2}
-    else:
-        response = {'Response': 'Invalid temp sent'}
-
-    # The object returned will be sent back as an HTTP message to the requester
-    return json.dumps(response)
 
 
 
@@ -197,11 +172,36 @@ if __name__ == '__main__':
     mailbox_password = args.p   # password
     mailbox_manager = mailboxManager.mailboxManager()
 
-    app.run(debug=False, host='rpi-jaeishin', port=5591)
+    app.run(debug=False, host='rpi-jaeishin', port=5593)
 
 
 
     while True:
+        app = Flask('RaspberryPi Mailbox Server')
+
+
+        @app.route('/send-mail', methods=['POST'])
+        def post_mail_callback():
+
+            global in_temp2
+
+            payload = request.get_json()
+            in_temp = int(payload["temp"])
+
+            # Print incomming temp
+            print("Incomming encrypted temp = " + str(in_temp))
+            in_temp2 = math.sqrt(in_temp)
+            print("Incomming decrypted temp = " + str(in_temp2))
+            print(payload)
+
+            if (in_temp2>60) and (in_temp2 <100):
+                mailbox_manager.add_mail(payload)
+                response = {'Response': 'Mail sent', 'Recieved': in_temp2}
+            else:
+                response = {'Response': 'Invalid temp sent'}
+
+            # The object returned will be sent back as an HTTP message to the requester
+            return json.dumps(response)
 
         # buzzer on buttom press and mode change
         button_status = grovepi.digitalRead(button)
