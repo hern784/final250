02f619ce9c5396c9c815ccdcff2678c16f0068f4
Richard Hernandez <hern784@usc.edu>
2019-05-01 21:48:12 -0700

    security

--- mailServer.py
+++ mailServer.py
@@ -9,35 +9,6 @@ import math
 
 app = Flask('RaspberryPi Mailbox Server')
 
-@app.route('/mailbox/search', methods=['GET'])
-def search_mailbox_callback():
-    password = request.args.get('password')
-    text = request.args.get('text')
-    field = request.args.get('field')
-    print(str(password) + str(text) + str(field))
-
-    if password == mailbox_password:
-        response = jsonify(mailbox_manager.get_mail(field, text))
-        payload = request.get_json()
-        in_temp = payload["temp"]
-        print(in_temp)
-        print(payload)
-
-        mailbox_manager.add_mail(payload)
-        response = {'Response': 'Mail sent'}
-
-    # The object returned will be sent back as an HTTP message to the requester
-        return json.dumps(response)
-        
-    else:
-        if password == None:
-            response = jsonify({'Response': 'Missing password'})
-
-        else:
-            response = jsonify({'Response': 'Password does not match'})
-
-    return response  
-
 @app.route('/send-mail', methods=['POST'])
 def post_mail_callback():
 
