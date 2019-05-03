f0415632cbb09ff8d8776c5cb4c99d16763bd4e8
Richard Hernandez <hern784@usc.edu>
2019-05-02 21:11:01 -0700

    complete

--- final.py
+++ final.py
@@ -165,7 +165,7 @@ def main():
                 i = 0
                 lcd.setRGB(0,122,0)
                 print("awake")
-                time.sleep(.5)
+                continue
                 flag = 0
             elif i == 5:
                 lcd.setRGB(0,0,0)
