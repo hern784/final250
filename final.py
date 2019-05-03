51b410bc0702935250e294affa940afd04c6870d
Richard Hernandez <hern784@usc.edu>
2019-05-02 20:38:30 -0700

    complete

--- final.py
+++ final.py
@@ -1,41 +1,53 @@
 import sys
 import time
-#sys.path.append('../../Software/Python/')
-#sys.path.append('../../Software/Python/grove_rgb_lcd')
 sys.path.append('/home/pi/Dexter/GrovePi/Software/Python')
 import grovepi
 import grove_rgb_lcd as lcd
 import math
-
-# api
 import requests
 import json
-
-# mqtt
 import paho.mqtt.client as mqtt
-
-#threading
 import threading
+from flask import Flask
+from flask import jsonify
+from flask import request
+import argparse
+
+#variables
+desired_temp_min = 60
+desired_temp_max = 100
+indoor_temp = 0
+outdoor_temp = 0
+temp = 0
+desired_temp = 70
+mode = 1
+hvac = 0
+i = 0
+flag = 0
+wind_on = "rpi-jaeishin/HVAC", "Entering wind mode: rpi-jaeishin"
+wind_off = "rpi-jaeishin/HVAC", "Exiting wind mode: rpi-jaeishin"
+
+#i/o being used
+
+sensor = 7
+buzzer_pin = 2
+button = 4
+potentiometer = 0    #
+grovepi.pinMode(buzzer_pin, "OUTPUT")
+grovepi.pinMode(button, "INPUT")
+grovepi.pinMode(potentiometer,"INPUT")
+lcd.setRGB(0,122,0)
+
+# Used for threading
 
 def startup():
     thread = threading.Thread(target=main)
     thread.daemon = True
     thread.start()
 
-
-
-# HTTP imports from mail Server
-
-from flask import Flask
-from flask import jsonify
-from flask import request
-
-import argparse
-import json
+# Used For HTTP requests
 
 app = Flask('RaspberryPi Mailbox Server')
-
-
 @app.route('/send-mail', methods=['POST'])
 def post_mail_callback():
 
@@ -54,58 +66,29 @@ def post_mail_callback():
         response = {'Response': 'Mail sent'}
     else:
         response = {'Response': 'Invalid temp sent'}
-    # The object returned will be sent back as an HTTP message to the requester
+
     return json.dumps(response)
 
-
-
-#i/o being used
-sensor = 7
-buzzer_pin = 2
-button = 4
-potentiometer = 0    #
-grovepi.pinMode(buzzer_pin, "OUTPUT")
-grovepi.pinMode(button, "INPUT")
-grovepi.pinMode(potentiometer,"INPUT")
-lcd.setRGB(0,122,0)
-
-#variables
-desired_temp_min = 60
-desired_temp_max = 100
-indoor_temp = 0
-outdoor_temp = 0
-temp = 0
-desired_temp = 70
-mode = 1
-hvac = 0
-i = 0
-wind_on = "rpi-jaeishin/HVAC", "Entering wind mode: rpi-jaeishin"
-wind_off = "rpi-jaeishin/HVAC", "Exiting wind mode: rpi-jaeishin"
-
-
-
+# Used for MQTT to send ack when entering or exiting wind mode
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
     client.subscribe("rpi-jaeishin/HVAC")
     client.message_callback_add("rpi-jaeishin/HVAC", ledcustom)
 
-
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
     print("on_message: msg.payload is of type " + str(type(msg.payload)))
 
-#changing temp with rotary encoder
+# changing temp with rotary encoder
+
 def get_rotary_angle():
-    ## from grove_rotary_angle_sensor.py
+
     adc_ref = 5
     grove_vcc = 5
     full_angle = 300
-    # Read sensor value from potentiometer
     sensor_value = grovepi.analogRead(potentiometer)
-    # Calculate voltage
     voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
-    # Calculate rotation in degrees (0 to 300)
     degrees = round((voltage * full_angle) / grove_vcc, 2)
 
     d_temp = 60 + degrees/5
@@ -113,23 +96,13 @@ def get_rotary_angle():
         d_temp = 100
     if d_temp < 60:
         d_temp = 60
+
     return d_temp
 
-# def rotary_temp():
-#     angle = grovepi.analogRead(potentiometer)
-#     r_temp = angle/5
-#     rot_temp = 60 + r_temp
-
-#     if rot_temp > 100:
-#         rot_temp = 100
-#     if rot_temp < 60:
-#         rot_temp = 60
-#     return rot_temp
-
+# Get indoor temp from grovepi sensor
 
 def get_indoor_temp():
     sensor = 7
-
     while True:
         try:
             [temp, humid] = grovepi.dht(sensor, 0)
@@ -144,8 +117,28 @@ def get_indoor_temp():
         except IOError:
             print ("Error")
 
-def main():
+# API for getting outdoor temp
 
+OWM_API_KEY = '5fa72aaa861d38b37e5ebd93634a1f88'  # OpenWeatherMap API Key
+DEFAULT_ZIP = 90089
+def get_weather(zip_code):
+    params = {
+        'appid': OWM_API_KEY,
+    }
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+ str(zip_code) + '&appid=' + str(OWM_API_KEY))
+    if response.status_code == 200: # Status: OK
+        data = response.json()
+        temp_k = data["main"]["temp"]
+        outdoor_temp = ((temp_k-273)*1.8+32)
+        return outdoor_temp
+    else:
+        print('error: got response code %d' % response.status_code)
+        print(response.text)
+        return 0.0, 0.0
+
+# main function with the logic for the thermostat
+
+def main():
     global desired_temp_min 
     global desired_temp_max    
     global indoor_temp 
@@ -157,14 +150,13 @@ def main():
     global wind_on
     global wind_off
     global i
+    global flag
 
     while True:
         try:
+            # Get indoor temp
             indoor_temp = int(get_indoor_temp())
-            # buzzer on buttom press and mode change
             
-
-
             # lcd sleep after 5 seonds
             time.sleep(.2)
 
@@ -175,10 +167,13 @@ def main():
                 lcd.setRGB(0,122,0)
             elif i == 25:
                 lcd.setRGB(0,0,0)
+                flag = 1
 
-        
-            if button_status:
-                              
+            # if lcd is off turn on, if lcd is on change mode and sound buzzer
+            if button_status and flag = 1:
+                lcd.setRGB(0,122,0)
+                flag = 0
+            else:             
                 grovepi.digitalWrite(buzzer_pin, 1)
                 time.sleep(.1)
                 grovepi.digitalWrite(buzzer_pin, 0)
@@ -195,15 +190,12 @@ def main():
                         client.publish("rpi-jaeishin/HVAC", str(wind_on))
                         print(wind_on)
                     hvac = "wind"
-             
-
                 else:
                     if hvac == "wind":
                         # send close window
                         client.publish("rpi-jaeishin/HVAC", str(wind_off))
                         print(wind_off)
                     hvac = "AC"
-                   
 
             if (indoor_temp < desired_temp):
                 if (outdoor_temp < desired_temp):
@@ -212,7 +204,6 @@ def main():
                         client.publish("rpi-jaeishin/HVAC", str(wind_off))
                         print(wind_off)
                     hvac = "heat"
-     
                 else:
                     if hvac != "wind":
                         # send open window
@@ -227,34 +218,31 @@ def main():
                     print(wind_off)
                 hvac = "fan"
 
-            #measure indoor and out door temp
+            # measure outdoor temp from API
             outdoor_temp = int(get_weather(DEFAULT_ZIP))
 
 
-            #Set LED to print in correct format
+            # Set LED to print in correct format
 
             #default
             if (mode == 1):
                 print ("\nmode = 1 - Default")
-
                 print("Temp: {:>3}F  {:>4}".format(indoor_temp, hvac))
                 print("Desired: {:>3}F".format(desired_temp))
                 lcd.setText_norefresh("Temp: {:>3}F {:>4}\nDesired: {:>3}F".format(indoor_temp, hvac, desired_temp))
+            
             #outdoor
             if (mode == 2):
-
                 print("\nmode = 2 - Outdoor")
                 print("Temp: {:>3}F {:>4}".format(indoor_temp, hvac))
                 print("Outdoor: {:>3}F".format(outdoor_temp))
                 lcd.setText_norefresh("Temp: {:>3}F {:>4}\nOutdoor: {:>3.2f}F".format(indoor_temp, hvac, outdoor_temp))
+            
             #edit
             if (mode == 0):
                 print("\nmode = 0 - Edit")
-
                 # get rotary angle set desired temp
                 desired_temp = get_rotary_angle()
-               
-
                 print("Set Temp: {:>3}F".format(desired_temp)) 
                 lcd.setText_norefresh("Set Temp:{:>3}F".format(desired_temp))
 
@@ -262,51 +250,7 @@ def main():
             lcd.setRGB(0,0,0)
             setText_norefresh("")
             break
-
-
-
-
-
-
-
-# TODO: Sign up for an API key
-OWM_API_KEY = '5fa72aaa861d38b37e5ebd93634a1f88'  # OpenWeatherMap API Key
-
-DEFAULT_ZIP = 90089
-
-def get_weather(zip_code):
-    params = {
-        'appid': OWM_API_KEY,
-        # TODO: referencing the API documentation, add the missing parameters for zip code and units (Fahrenheit)
-    }
-
-    response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+ str(zip_code) + '&appid=' + str(OWM_API_KEY))
-
-    if response.status_code == 200: # Status: OK
-        data = response.json()
-        temp_k = data["main"]["temp"]
-        outdoor_temp = ((temp_k-273)*1.8+32)
-        #hum = data["main"]["humidity"]
-        # TODO: Extract the temperature & humidity from data, and return as a tuple
-        return outdoor_temp
-
-    else:
-        print('error: got response code %d' % response.status_code)
-        print(response.text)
-        return 0.0, 0.0
-
-def lcd_sleep():
-    i = 0
-    button_status = grovepi.digitalRead(button)
-    while i < 4:
-        i = i + 1
-        time.sleep(1)
-        if button_status:
-            i = 0
-    else:
-        lcd.setRGB(0,0,0)
-
-
+            
 ##########################  begin  #######################################
 
 if __name__ == '__main__':
@@ -318,8 +262,6 @@ if __name__ == '__main__':
     client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
     client.loop_start()
     i = 0
-
-    # HTTP from mailServer
 
     # Set up argparse, a Python module for handling command-line arguments
     parser = argparse.ArgumentParser(prog='mailServer',
